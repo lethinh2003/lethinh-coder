75299ff5b5f1646c37e1d87ef3774396f9e71375
lethinh2003 <muradvn2003@gmail.com>
2022-06-07 23:01:19 +0700

    fix meta tag name

--- pages/_app.js
+++ pages/_app.js
@@ -26,19 +26,19 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       <SessionProvider session={session} refetchOnWindowFocus={false}>
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <title>LT Blog - Le Thinh Blog</title>
           <link rel="icon" type="image/x-icon" href="https://i.imgur.com/U0BdIic.png" />
+          <meta name="author" content="Thinh Le" />
+          {/* <title>LT Blog - Le Thinh Blog</title>
 
           <meta name="description" content="LT Blog - Le Thinh Blog" />
           <meta
             name="keywords"
             content="Thinhle, Le Thinh, lethinh2003, huynhobe, blog, laptrinhvien, it, cntt, source code free, source, code, game freefire, code game freefire, vong quay freefire, javascript"
           />
-          <meta name="author" content="Thinh Le" />
           <meta property="og:title" content="LT Blog - Le Thinh Blog" />
 
           <meta property="og:description" content="" />
-          <meta property="og:type" content="website" />
+          <meta property="og:type" content="website" /> */}
 
           <meta property="og:image:width" content="1280" />
           <meta property="og:image:height" content="720" />
